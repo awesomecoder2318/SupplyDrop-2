7b06838c1be193baca22f8bd9e94f020de2289e2
awesomecoder2318 <84549703+awesomecoder2318@users.noreply.github.com>
2021-06-17 16:36:38 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground, boxBody1, boxBody2, boxBody3
+var boxBodyA,boxBodyB,boxBodyC
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	boxBodyC = Bodies.rectangle(400,650, 200, 20 , {isStatic:true} );
+ 	World.add(world, boxBodyC);
+	  boxBodyB = Bodies.rectangle(500,610, 20, 100 , {isStatic:true} );
+ 	World.add(world, boxBodyB);
+	 boxBodyA = Bodies.rectangle(300,610, 20, 100 , {isStatic:true} );
+ 	World.add(world, boxBodyA);
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, packageBody);
+
+	
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+	boxBody1=createSprite(300,610,20,100)
+	boxBody1.shapeColor="red"
+	boxBody2=createSprite(500,610,20,100)
+	boxBody2.shapeColor="red"
+	boxBody3=createSprite(400,650,200,20)
+	boxBody3.shapeColor="red"
+
+	
+	
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	boxBody1.shapeColor="red"
+
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  boxBody1.x= boxBodyA.position.x 
+  boxBody1.y= boxBodyA.position.y 
+  boxBody2.x= boxBodyB.position.x 
+  boxBody2.y= boxBodyB.position.y 
+  boxBody3.x= boxBodyC.position.x 
+  boxBody3.y= boxBodyC.position.y 
+  drawSprites();
+  if (keyDown("down")){
+	  Matter.Body.setStatic(packageBody,false)
+	  Matter.Body.setStatic(boxBodyA,true)
+	  Matter.Body.setStatic(boxBodyB,true)
+	  Matter.Body.setStatic(boxBodyC,true)
+  }
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
+    
+  }
+}
+
+
+
